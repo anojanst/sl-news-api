cf5a5ee0b0fda2eb3513abda8f44c349d7eda7da
Anojan ST <ethanojan1@gmail.com>
2022-04-08 17:20:00 +0530

    test 3

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 from fastapi import FastAPI
-from routes.index import newsApi
 app = FastAPI()
 
 
 @app.get('/', tags=['Home'])
 def home():
     return {'pageName': "Homepage"}
-
-
-app.include_router(newsApi)
--- schemas/index.py
+++
@@ -1,1 +0,0 @@
-from schemas.todo import Todo
